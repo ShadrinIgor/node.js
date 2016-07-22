b57cddef8b0710e3656c924d4eee1d2d652869be
Shadrin Igor <shadrin.igor@gmail.com>
2016-07-22 12:31:15 +0500

    Сборка проекта через Gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var util = require('gulp-util');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task( 'clear', function(){
+    return gulp.src()
+});
