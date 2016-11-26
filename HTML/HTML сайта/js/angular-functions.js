3359347b5f1428de85718e1c6cec096f0b7e010f
Shadrin Igor <shadrin.igor@gmail.com>
2016-11-27 01:15:49 +0500

    Начала верстки

--- HTML/HTML сайта/js/angular-functions.js
+++
@@ -1,7 +0,0 @@
-angular.module( "main-ui", [] )
-    .controller( 'main-articles', function( $scope ){
-        $scope.name = 'Ура получилось';
-    })
-    .controller( 'main-menu', function( $scope ){
-
-    })
